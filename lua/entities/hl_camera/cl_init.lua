519ff4bc207a4da2c2828cae1e1e32d0602bfbcd
NeatNit Gralporit <NeatNit Gralporit>
2017-03-18 17:38:03 +0200

    move more stuff from shared to init and cl_init

--- lua/entities/hl_camera/cl_init.lua
+++ lua/entities/hl_camera/cl_init.lua
@@ -1,2 +1,142 @@
 include("shared.lua")
 include("language.lua")
+
+local ENT = ENT	-- so we can use ENT within hooks and functions
+local dev = GetConVar("developer")
+
+local trapping_camera
+net.Receive("hl_camera_key", function(len)
+	-- we are being updated with a new key
+	local camera = net.ReadEntity()
+	if not IsValid(camera) or camera:GetClass() ~= ENT.ClassName then return end
+
+	camera.AssignedKey.key = net.ReadInt(10)
+	camera.AssignedKey.toggle = net.ReadBool()
+
+	camera:UpdateText()
+end)
+
+function ENT:UpdateText()
+	local keyname = input.GetKeyName(self.AssignedKey.key)
+	self.AssignedKey.text = keyname and language.GetPhrase(keyname)
+end
+
+function ENT:GetText()
+	if trapping_camera == self then return "Press any key" end
+	return self.AssignedKey.text or "#hl_camera.unassigned"
+end
+
+function ENT:StartSetNewKey()
+	if IsValid(trapping_camera) then
+		trapping_camera:CancelSetNewKey()
+	end
+
+	trapping_camera = self
+	if not input.IsKeyTrapping() then input.StartKeyTrapping() end
+end
+
+function ENT:CancelSetNewKey()
+
+end
+
+function ENT:Think()
+	if trapping_camera == self and input.IsKeyTrapping() then
+		local key = input.CheckKeyTrapping()
+		if key then
+			if key ~= KEY_ESCAPE then
+				net.Start("hl_camera_key")
+				net.WriteEntity(self)
+				net.WriteInt(key, 10)
+				net.WriteBool(self.AssignedKey.toggle)
+				net.SendToServer()
+
+				-- change the setting internally, just so the checkbox appears correct if you right-click very fast after toggling (i.e. before the server updated us)
+				self.AssignedKey.key = key
+				self:UpdateText()
+			end
+
+			trapping_camera = nil
+		end
+	end
+end
+
+function ENT:Draw()
+	-- obey cl_drawcameras that is used in vanilla cameras
+	if not cvars.Bool("cl_drawcameras", true) then return end
+
+	-- don't draw cameras while looking through another camera
+	if GetViewEntity():GetClass() == ENT.ClassName then return end
+
+	-- don't draw cameras while using the camera swep
+	local wep = LocalPlayer():GetActiveWeapon()
+	if IsValid(wep) and wep:GetClass() == "gmod_camera" then return end
+
+	-- in all other cases, draw
+	self:DrawModel()
+end
+
+-- draw the label, if we're looking at this camera
+function ENT:DrawTranslucent()
+	-- obey cl_drawcameras that is used in vanilla cameras
+	if not cvars.Bool("cl_drawcameras", true) then return end
+
+	if not self.StartLookTime or (LocalPlayer():GetEyeTrace().Entity ~= self and trapping_camera ~= self) then
+		self.StartLookTime = RealTime()
+		return
+	end
+
+	-- must be aiming at this camera for at least 0.7 second
+	if trapping_camera ~= self and RealTime() < self.StartLookTime + 0.7 then return end
+
+	local screenpos = self:GetPos():ToScreen()
+	if not screenpos.visible then return end
+
+	cam.Start2D()
+		surface.SetFont("DermaDefaultBold")
+		local w, h = surface.GetTextSize(self:GetText())
+		screenpos.x = screenpos.x - (w / 2)
+		screenpos.y = screenpos.y - (h / 2)
+
+		local verts = {
+			{ x = screenpos.x + w, y = screenpos.y - 2 },
+			{ x = screenpos.x + w + 3, y = screenpos.y },
+			{ x = screenpos.x + w + 3, y = screenpos.y + h },
+			{ x = screenpos.x + w, y = screenpos.y + h + 2 },
+			{ x = screenpos.x, y = screenpos.y + h + 2 },
+			{ x = screenpos.x - 3, y = screenpos.y + h },
+			{ x = screenpos.x - 3, y = screenpos.y },
+			{ x = screenpos.x, y = screenpos.y - 2 }
+		}
+
+		surface.SetDrawColor(220, 220, 220)
+		draw.NoTexture()
+		surface.DrawPoly(verts)
+
+		surface.SetTextPos(screenpos.x, screenpos.y)
+		surface.SetTextColor(10, 10, 10)
+		surface.DrawText(self:GetText())
+	cam.End2D()
+end
+
+function ENT:CalcView(ply, origin, angles, fov, znear, zfar)
+	local view = {
+		origin = self:GetPos(),
+		angles = self:GetAngles() + Angle(0, 0, self:GetRoll()),
+		znear = self:GetNearZ() > 0 and self:GetNearZ() or znear,
+		zfar = self:GetFarZ() > 0 and self:GetFarZ() or zfar,
+		fov = self:GetFOV() > 0 and self:GetFOV() or fov
+	}
+
+	if view.znear <= 0 then view.znear = znear end
+	if view.zfar <= 0 then view.zfar = zfar end
+	if view.fov <= 0 then view.fov = fov end
+
+	return view
+end
+
+hook.Add("CalcView", ENT.Folder, function(ply, ...)
+	local camera = ply:GetViewEntity()
+	if IsValid(camera) and camera:GetClass() == ENT.ClassName then
+		return camera:CalcView(ply, ...)
+	end
+end)
--- lua/entities/hl_camera/init.lua
+++ lua/entities/hl_camera/init.lua
@@ -1,3 +1,141 @@
 AddCSLuaFile("cl_init.lua")
 AddCSLuaFile("language.lua")
 include("shared.lua")
+
+util.AddNetworkString("hl_camera_key")
+
+local ENT = ENT	-- so we can use ENT within hooks and functions
+local dev = GetConVar("developer")
+DEFINE_BASECLASS(ENT.Base)
+
+-- The client is either probing us because it doesn't know which key is assigned to the camera
+-- or it's telling us that it wants to assign a new key (or change the toggle setting)
+net.Receive("hl_camera_key", function(len, ply)
+	local camera = net.ReadEntity()
+
+	if len <= 16 then
+		camera:UpdatePlayer(ply)
+		return
+	end
+
+	local key = net.ReadInt(10)
+	local toggle = net.ReadBool()
+	camera:SetPlayerKey(ply, key, toggle)
+end)
+
+
+-- Send the
+function ENT:UpdatePlayer(ply)
+	-- gotta give it a small delay, otherwise the net message is sent too early
+	-- and received before the client has initialized the entity
+	-- (in the case of calling this method immediately after spawning)
+	timer.Simple(0.1, function()
+		net.Start("hl_camera_key")
+		net.WriteEntity(self)
+
+		local steamid = ply:SteamID()
+		if not self.PlayerBinds[steamid] then
+			net.WriteInt(KEY_NONE, 10)
+			net.WriteBool(false)
+		else
+			net.WriteInt(self.PlayerBinds[steamid].key, 10)
+			net.WriteBool(self.PlayerBinds[steamid].toggle)
+		end
+		net.Send(ply)
+	end)
+end
+
+-- register numpad functions
+numpad.Register("hl_camera_on", function(ply, camera)
+	if not IsValid(camera) then return false end
+	ply:SetViewEntity(camera)
+end)
+numpad.Register("hl_camera_off", function(ply, camera)
+	if not IsValid(camera) then return false end
+	if ply:GetViewEntity() == camera then ply:SetViewEntity(nil) end
+end)
+numpad.Register("hl_camera_toggle", function(ply, camera)
+	if not IsValid(camera) then return false end
+	if ply:GetViewEntity() == camera then
+		ply:SetViewEntity(nil)
+	else
+		ply:SetViewEntity(camera)
+	end
+end)
+
+-- lookup table for each player's assigned camera for a certain key
+-- saved globally to stop glitches during development
+HL_CAMERA_PKC = HL_CAMERA_PKC or {}
+local PlayerKeyCamera = HL_CAMERA_PKC
+
+function ENT:SetPlayerKey(ply, key, toggle)
+	steamid = ply:SteamID()	-- get player's SteamID
+
+	-- remove the existing bind for this player for this camera, if one exists
+	if self.PlayerBinds[steamid] then
+
+		for _, impulseID in ipairs(self.PlayerBinds[steamid].impulse) do
+			numpad.Remove(impulseID)
+		end
+
+		-- remove from tracking table and our own table
+		local oldkey = self.PlayerBinds[steamid].key
+		PlayerKeyCamera[steamid][oldkey] = nil
+		self.PlayerBinds[steamid] = nil
+	end
+
+	if	-- validity check. if key isn't a valid key, just unbind the camera.
+		not key
+		or key < KEY_FIRST
+		or key == KEY_NONE
+		or key > BUTTON_CODE_LAST
+	then
+		-- make sure player isn't stuck viewing through us
+		if ply:GetViewEntity() == self then ply:SetViewEntity(nil) end
+
+		self:UpdatePlayer(ply)
+		return
+	end
+
+	-- make sure we have this player in our lookup table
+	PlayerKeyCamera[steamid] = PlayerKeyCamera[steamid] or {}
+
+	-- If the player has another camera assigned to this key, unassign it
+	if IsValid(PlayerKeyCamera[steamid][key]) then
+		PlayerKeyCamera[steamid][key]:SetPlayerKey(ply, nil)
+	end
+
+	-- Assign the player
+	toggle = tobool(toggle)	-- don't want nil or non-boolean crap
+	self.PlayerBinds[steamid] = {}
+	self.PlayerBinds[steamid].key = key
+	self.PlayerBinds[steamid].toggle = toggle
+	self.PlayerBinds[steamid].impulse = {}
+	PlayerKeyCamera[steamid][key] = self
+
+	timer.Simple(0.1, function()
+		if toggle then
+			table.insert(self.PlayerBinds[steamid].impulse, numpad.OnDown(ply, key, "hl_camera_toggle", self))
+		else
+			table.insert(self.PlayerBinds[steamid].impulse, numpad.OnDown(ply, key, "hl_camera_on", self))
+			table.insert(self.PlayerBinds[steamid].impulse, numpad.OnUp(ply, key, "hl_camera_off", self))
+		end
+	end)
+	self:UpdatePlayer(ply)
+end
+
+function ENT:OnRemove()
+	for _, ply in pairs(player.GetAll()) do
+		if ply:GetViewEntity() == self then
+			ply:SetViewEntity(nil)
+		end
+	end
+end
+
+function ENT:PhysicsUpdate(phys)
+	-- If we're not being held by the physgun, we should be frozen.
+	if not self:IsPlayerHolding() then
+		phys:EnableMotion(false)
+		phys:Sleep()
+	end
+end
