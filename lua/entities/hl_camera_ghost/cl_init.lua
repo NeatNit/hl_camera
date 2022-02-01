70d925a266f1f2faef53ced262fcaae100dcb377
penolakushari <45951611+penolakushari@users.noreply.github.com>
2022-02-01 06:34:08 +0300

    Fixed camera ghosts causing lua errors when loading saves with them

--- lua/entities/hl_camera_ghost/cl_init.lua
+++ lua/entities/hl_camera_ghost/cl_init.lua
@@ -2,7 +2,7 @@ include("shared.lua")
 
 function ENT:DrawTranslucent()
 	local camera = self:GetParent()
-	if not camera then return end
+	if not IsValid(camera) then return end
 	if not camera:ShouldDraw() then return end
 
 	-- in all other cases, draw
--- lua/entities/hl_camera_ghost/init.lua
+++ lua/entities/hl_camera_ghost/init.lua
@@ -2,3 +2,6 @@ include("shared.lua")
 
 AddCSLuaFile( "cl_init.lua" )
 AddCSLuaFile( "shared.lua" )
+
+ENT.DisableDuplicator = true
+ENT.DoNotDuplicate  = true
